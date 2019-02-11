57500018841df53718f7dfe3693b52d4c7a118c0
Alvaro Bonilla <alvarobonillavillalba@gmail.com>
2019-02-11 18:01:26 +0100

    endPoints second

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,34 @@ router.get('/profile/:id', ensureLogin.ensureLoggedIn('/auth/login'), (req, res,
   res.render('profile')
 });
 
-router.put('/profile/:id', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
+router.put('/profile', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
   
 });
 
 router.get('/xperience', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
   res.render('xperience')
 });
+
+router.post('/xperience', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
+  
+});
+
+router.put('/xperience/:id', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
+  
+});
+
+router.delete('/xperience/:id', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
+  
+});
+
+router.get('/xperience-list', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
+  res.render('xperience-list')
+});
+
+router.get('/users-list', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
+  res.render('users-list')
+});
+
+
 
 module.exports = router;
