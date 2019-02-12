df69c9dc52a6dd9e18f496cc67ff6fa699fcdc0c
Dani Garcia <danielgarciamartinez1985@gmail.com>
2019-02-12 12:46:56 +0100

    profile endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,56 @@
 const express = require('express');
 const router  = express.Router();
 const ensureLogin = require('connect-ensure-login')
+const User = require('../models/User')
+
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
 
 /* GET home page */
 router.get('/', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next) => {
   res.render('index');
 });
 
+router.get('/profile', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next) =>  {
+  res.render('profile');
+})
+
 router.get('/profile/:id', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
-  res.render('profile')
+    User.findById(req.params.id)
+    .then(users => {
+      res.render('profile', {users})
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
-router.put('/profile', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
+router.post('/profile', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
+  let username = req.body.username
+  let password = req.body.password
   
-});
+  if (username === "" || password === "") {
+    res.render("profile", { message: "Indicate username and password" });
+    return;
+  }
+  
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+  
+  const profileUpdate = {
+    username,
+    password: hashPass
+  }
+  User.findByIdAndUpdate(req.user._id, profileUpdate)
+  .then(users => {
+  res.render('profile', {users})
+  })
+  .catch(error => {
+  console.log(error)
+  });
+})
+
 
 router.get('/xperience', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next)=>{
   res.render('xperience')
