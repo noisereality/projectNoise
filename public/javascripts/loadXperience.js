c4f6174719a9865b998ea45f941d0175e2171d84
Alvaro Bonilla <alvarobonillavillalba@gmail.com>
2019-02-13 18:56:30 +0100

    esto suena

--- public/javascripts/loadXperience.js
+++ public/javascripts/loadXperience.js
@@ -1,16 +1,13 @@
+let multi,xper;
+let index = 0;
+let rows;
+let loops;
+
 function loadXperience(xperience){
   let xperienceEnt = document.querySelector("#xperience-ent");
   xperienceEnt.innerHTML = "";
   let loopEnt
   let newBox
-  // newBox.setAttribute("class", "active");
-  // newBox.object3D.position.set(0, 10, -10);
-  //color="#CCCCCC"
-  //newBox.setAttribute('position', {x: 0, y: 10, z: -10});
-  // newBox.setAttribute('color', "#CCCCCC");
-  //newBox.setAttribute('position', {x: 1, y: 2, z: 3});
-  //xperienceEnt.appendChild(newBox); 
-  let xPos, yPos, zPos = 0
   xperience.loops.forEach((loop,index) => {
 
     loopEnt = document.createElement("a-entity")
@@ -32,56 +29,36 @@ function loadXperience(xperience){
     xperienceEnt.appendChild(loopEnt); 
   });
 
-  let buffer;
-  let buffers = new Tone.Buffers()
-  xperience.loops.forEach((loop,index) => {
-    buffer = new Tone.Buffer(loop.sample.url, function(){
-      buffers.add(loop.sample.name,loobuffer);
-    });
-
-    notes.push({
-      name: loop.sample.name,
-      player: new Tone.Player(loop.sample.url).toMaster()});
-  });
-  var pianoSamples = new Tone.Buffers({
-    "C4" : "path/to/C4.mp3",
-    "C#4" : "path/to/C#4.mp3",
-    "D4" : "path/to/D4.mp3",
-    "D#4" : "path/to/D#4.mp3",
-  }, function(){
-    //play one of the samples when they all load
-    player.buffer = pianoSamples.get("C4");
-    player.start();
-  });
-
-  loadSounds(xperience)
- 
+  loadData(xperience)
   
 }
 
-function loadSounds(xperience){
-  // var synth = new Tone.MembraneSynth().toMaster()
-  //var sampler = new Tone.Sampler({A2:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/969699/808-kick-vh.mp3"})
-  var player = new Tone.Player("https://s3-us-west-2.amazonaws.com/s.cdpn.io/969699/808-kick-vh.mp3").toMaster();
+function loadData(xperience){
 
-  let notes = []
-
+  let players = new Object;
   xperience.loops.forEach((loop,index) => {
-    notes.push({
-      name: loop.sample.name,
-      player: new Tone.Player(loop.sample.url).toMaster()});
+    players[loop.sample.name] = loop.sample.url;
   });
 
-  const rows = document.querySelectorAll("a-box")
-  const loops = document.querySelectorAll(".loop-ent")
-  //notes = ['G5', 'C5', 'F4', 'A2'];
-  let index = 0;
+  var multiPlayer = new Tone.Players(players, function(){
+    startSounds(multiPlayer,xperience).toMaster()
+  });
 
-  Tone.Transport.scheduleRepeat(repeat, '16n');
+}
+
+
+function startSounds(multiPlayer,xperience){
+  rows = document.querySelectorAll("a-box")
+  loops = document.querySelectorAll(".loop-ent")
+  multi = multiPlayer;
+  xper = xperience;
+  Tone.Transport.scheduleRepeat(repeatLoop, '16n');
   Tone.Transport.start();
 
-  function repeat(time) {
+}
 
+function repeatLoop(time) {
+  
     let step = index % 16;
     let box;
 
@@ -99,24 +76,11 @@ function loadSounds(xperience){
       row=rows[step]
       box = loop.querySelector(`a-box:nth-child(${step + 1})`);
       if(box.className === 'active'){
-        console.log(notes.find(note => note.name === box.getAttribute("sound")).player)
-        //console.log(xperience.loops.find(loop => loop.sample.name === box.getAttribute("sound")).sample.url)
-        //notes[box.className].start();[box.getAttribute("sound")]
-        notes.find(note => note.name === box.getAttribute("sound")).player.start()
+        multi.get(box.getAttribute("sound")).toMaster().start()
         box.setAttribute('color', '#222288');
       }else{
         box.setAttribute('color', '#667766');
       }
-
     })
-      
     index++
-
-   
-
-
-
-     
-    
-  }
 }
